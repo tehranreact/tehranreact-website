0d37493436ff24ae830fd433654f1761327dd1cf
Kiarash Zarinmehr <kiarash.zar@gmail.com>
2019-07-22 16:15:00 +0430

    Remove layout width limit

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,14 +12,7 @@ import './layout.css';
 
 const Layout = ({ children }) => {
   return (
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0px 1.0875rem 1.45rem`,
-        paddingTop: 0,
-      }}
-    >
+    <div>
       <main>{children}</main>
     </div>
   );
